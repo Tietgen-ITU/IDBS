440ee2df8a6117736ba387e5db3a9a9a82772884
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-09-16 13:14:29 +0200

    Add count to Answer 08

--- Homework/Homework-01/Answers/Answer-08.sql
+++ Homework/Homework-01/Answers/Answer-08.sql
@@ -3,17 +3,18 @@
 * the category ‘Newsworthy’ is 156. How many persons have had some role in some
 * movie from all genres from the category ‘Lame’?
 */
+SELECT COUNT(*)
+FROM (SELECT I.personid, COUNT(DISTINCT MG.genre)
+        FROM involved I
+        INNER JOIN movie_genre MG ON MG.movieid = I.movieid
+        WHERE MG.genre IN (SELECT G.genre
+                            FROM genre G
+                            WHERE G.category = 'Lame')
+        GROUP BY I.personid
+        HAVING COUNT(DISTINCT MG.genre) = (SELECT COUNT(G.genre)
+                                            FROM genre G
+                                            WHERE G.category = 'Lame')) Result;
 
-SELECT I.personid, COUNT(DISTINCT MG.genre)
-FROM involved I
-INNER JOIN movie_genre MG ON MG.movieid = I.movieid
-WHERE MG.genre IN (SELECT G.genre
-                    FROM genre G
-                    WHERE G.category = 'Lame')
-GROUP BY I.personid
-HAVING COUNT(DISTINCT MG.genre) = (SELECT COUNT(G.genre)
-                                    FROM genre G
-                                    WHERE G.category = 'Lame');
 
 /*Anwers: 1*/
 
