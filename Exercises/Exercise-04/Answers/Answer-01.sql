108918b729a4ff981a306ee0e8c3c1ba426c1e83
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-09-23 10:53:39 +0200

    Task 01 completed

---
+++ Exercises/Exercise-04/Answers/Answer-01.sql
@@ -0,0 +1,13 @@
+/*
+Create	a	view	AllAccountRecords	that	joins	the	Accounts	and	
+AccountRecords	and	shows	one	entry	for	each	record	for	each account.
+The	view	should	show	all	columns	from	both	tables,	first	Accounts	and	
+then	AccountRecords,	except	the	AccountRecords.AID	column. Accounts	
+with	no	entry	in	AccountRecords should	be	shown	with	NULLs	in	the	
+columns	for	AccountRecords.
+*/
+
+Create VIEW AllAccountRecords AS
+SELECT A.*, AR.rid, AR.rdate, AR.rtype, AR.ramount, AR.rbalance
+FROM accounts A 
+FULL JOIN AccountRecords AR ON AR.aid = A.aid
