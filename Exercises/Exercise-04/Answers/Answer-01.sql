81724b66a75ef75d93418173de049b9c3bd12dae
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-09-23 16:51:39 +0200

    Fix answer 1

--- Exercises/Exercise-04/Answers/Answer-01.sql
+++ Exercises/Exercise-04/Answers/Answer-01.sql
@@ -7,7 +7,9 @@ with	no	entry	in	AccountRecords should	be	shown	with	NULLs	in	the
 columns	for	AccountRecords.
 */
 
+DROP VIEW IF EXISTS AllAccountRecords;
+
 Create VIEW AllAccountRecords AS
 SELECT A.*, AR.rid, AR.rdate, AR.rtype, AR.ramount, AR.rbalance
 FROM accounts A 
-FULL JOIN AccountRecords AR ON AR.aid = A.aid
+LEFT OUTER JOIN AccountRecords AR ON AR.aid = A.aid
