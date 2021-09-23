cd1551ed1b1806777e030f2ab70f1ab25b85f572
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-09-23 16:26:17 +0200

    Complete answer 03

--- Exercises/Exercise-04/Answers/Answer-03.sql
+++ Exercises/Exercise-04/Answers/Answer-03.sql
@@ -6,7 +6,26 @@ the	balance	on	the	account and	c)	installs the	correct	balance	into	the
 AccountRecords	entry.
 */
 
+
+DROP FUNCTION IF EXISTS CheckRecord();
+DROP TRIGGER IF EXISTS CheckRecord ON accountrecords;
+
+CREATE FUNCTION CheckRecord()
+RETURNS TRIGGER
+AS $$ BEGIN
+    
+    IF (NEW.ramount < (SELECT (A.abalance + A.aover) as balance  FROM accounts A WHERE A.aid = NEW.aid))  THEN
+        RAISE EXCEPTION 'Not a sufficent amount in balance' USING ERRCODE = '45000';
+    END IF;
+
+    UPDATE accounts 
+    SET abalance = abalance + NEW.ramount, adate = CURRENT_DATE
+    WHERE aid = NEW.aid;
+
+    return NEW;
+END; $$ LANGUAGE plpgsql;
+
 CREATE TRIGGER CheckRecord
 BEFORE INSERT
-ON AccountRecords
-
+ON accountrecords
+FOR EACH ROW EXECUTE PROCEDURE CheckRecord();
