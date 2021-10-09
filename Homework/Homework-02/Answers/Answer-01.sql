8e75c34fffb00e2af918864d4451393335b8a86c
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-10-09 15:02:17 +0200

    Solved Answer-01

---
+++ Homework/Homework-02/Answers/Answer-01.sql
@@ -0,0 +1,9 @@
+/*
+    (a) The empire `Great Britain' consists of 4 countries. 
+    How many countries does the empire `Danish Empire' consist of?
+*/
+
+SELECT COUNT(*)
+FROM (SELECT E.countryCode, E.empire
+FROM empires E
+WHERE E.empire = 'Danish Empire') R;
