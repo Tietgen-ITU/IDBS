cea4237eac50b7a52b7f88b1d2a1b314655eb521
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-11-03 21:42:27 +0100

    Add constraints to DDL in task 1

--- Homework/Homework-03/Part-01/create.sql
+++ Homework/Homework-03/Part-01/create.sql
@@ -1,11 +1,11 @@
 -- First drop tables if they don't exists...
 DROP TABLE IF EXISTS Reviews;
-DROP TABLE IF EXISTS Grant;
+DROP TABLE IF EXISTS "Grant";
 DROP TABLE IF EXISTS Sponsor;
 DROP TABLE IF EXISTS Monitors;
 DROP TABLE IF EXISTS Party;
-DROP TABLE IF EXISTS Role;
 DROP TABLE IF EXISTS ServeIn;
+DROP TABLE IF EXISTS Role;
 DROP TABLE IF EXISTS Participate;
 DROP TABLE IF EXISTS Linking;
 DROP TABLE IF EXISTS Opposes;
@@ -17,11 +17,11 @@ DROP TABLE IF EXISTS People;
 -- Create tables
 
 CREATE TABLE People (
-    ID INT NOT NULL PRIMARY KEY SERIAL,
-    Name VARCHAR(max),
-    Address VARCHAR(max),
-    Phone VARCHAR(8),
-    DOB DATE,
+    ID SERIAL NOT NULL PRIMARY KEY,
+    Name VARCHAR NOT NULL,
+    Address VARCHAR NOT NULL,
+    Phone VARCHAR(8) NOT NULL,
+    DOB DATE NOT NULL,
     DOD DATE
 );
 
@@ -35,26 +35,26 @@ CREATE TABLE Enemy (
 );
 
 CREATE TABLE Asset (
-    Name VARCHAR(max) NOT NULL,
+    Name VARCHAR NOT NULL,
     MemberID INT NOT NULL REFERENCES Member(PID),
-    Detail VARCHAR(MAX),
-    Uses VARCHAR(MAX),
+    Detail VARCHAR NOT NULL,
+    Uses VARCHAR NOT NULL,
     PRIMARY KEY (Name, MemberID)
 );
 
 CREATE TABLE Opposes (
     MID INT NOT NULL REFERENCES Member(PID),
     EID INT NOT NULL REFERENCES Enemy(PID),
-    Start_date DATE,
+    Start_date DATE NOT NULL,
     End_date Date,
     PRIMARY KEY (MID, EID)
 );
 
 CREATE TABLE Linking (
-    ID INT NOT NULL PRIMARY KEY SERIAL,
-    Name VARCHAR(MAX),
-    Type VARCHAR(MAX),
-    Description VARCHAR(MAX)
+    ID SERIAL NOT NULL PRIMARY KEY,
+    Name VARCHAR NOT NULL,
+    Type VARCHAR NOT NULL,
+    Description VARCHAR NOT NULL
 );
 
 CREATE TABLE Participate (
@@ -64,23 +64,24 @@ CREATE TABLE Participate (
 );
 
 CREATE TABLE Role (
-    ID INT NOT NULL PRIMARY KEY SERIAL,
-    Title VARCHAR(MAX)
+    ID SERIAL NOT NULL PRIMARY KEY,
+    Title VARCHAR NOT NULL UNIQUE
 );
 
 CREATE TABLE ServeIn (
     RID INT NOT NULL REFERENCES Role(ID),
     MID INT NOT NULL REFERENCES Member(PID),
-    Start_date DATE,
-    End_date DATE,
-    Salary INT,
+    Start_date DATE NOT NULL,
+    End_date DATE NOT NULL,
+    Salary INT NOT NULL,
     PRIMARY KEY (RID, MID)
 );
 
 CREATE TABLE Party (
-    ID INT NOT NULL PRIMARY KEY SERIAL,
-    Country VARCHAR(MAX),
-    Name VARCHAR(MAX)
+    ID SERIAL NOT NULL PRIMARY KEY,
+    Country VARCHAR,
+    Name VARCHAR,
+    UNIQUE(Country, Name)
 );
 
 
@@ -88,32 +89,32 @@ CREATE TABLE Monitors (
     PartyID INT NOT NULL REFERENCES Party(ID),
     MID INT NOT NULL REFERENCES Member(PID),
     Start_date DATE NOT NULL,
-    End_date DATE,
+    End_date DATE NOT NULL,
     PRIMARY KEY (PartyID, MID, Start_date)
 );
 
 CREATE TABLE Sponsor (
-    ID INT NOT NULL PRIMARY KEY SERIAL,
-    Name VARCHAR(MAX),
-    Address VARCHAR(MAX),
-    Industry VARCHAR(MAX)
+    ID SERIAL NOT NULL PRIMARY KEY,
+    Name VARCHAR NOT NULL,
+    Address VARCHAR NOT NULL,
+    Industry VARCHAR NOT NULL
 );
 
-CREATE TABLE Grant (
-    ID INT NOT NULL PRIMARY KEY SERIAL,
+CREATE TABLE "Grant" (
+    ID SERIAL NOT NULL PRIMARY KEY,
     SID INT NOT NULL REFERENCES Sponsor(ID),
     MID INT NOT NULL REFERENCES Member(PID),
     Date DATE NOT NULL,
-    Amount INT,
-    PayBack INT,
+    Amount INT NOT NULL,
+    PayBack INT NOT NULL,
 
     UNIQUE(SID, MID, Date)
 );
 
 CREATE TABLE Reviews (
-    GID INT NOT NULL REFERENCES Grant(ID),
-    MID INT NOT NULL REFERENCES Member(PID)
-    Date DATE,
-    Grade CHAR(1),
+    GID INT NOT NULL REFERENCES "Grant"(ID),
+    MID INT NOT NULL REFERENCES Member(PID),
+    Date DATE NOT NULL,
+    Grade INT,
     PRIMARY KEY (GID, MID)
 );
