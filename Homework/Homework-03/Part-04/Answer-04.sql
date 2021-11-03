70a51c2f694dc3007ae776f781241e6c0985b965
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-11-03 21:16:50 +0100

    Solve 4.4

---
+++ Homework/Homework-03/Part-04/Answer-04.sql
@@ -0,0 +1,14 @@
+CREATE VIEW CountryRatio AS
+SELECT cc.code, cc.Population, SUM(c.population), CAST(cc.population as DECIMAL) / SUM(c.population) ratio
+FROM countries cc
+INNER JOIN cities c ON c.countrycode = cc.code
+GROUP BY cc.code;
+
+
+SELECT *
+FROM CountryRatio as cr
+WHERE cr.sum > 1000000
+ORDER BY ratio DESC
+LIMIT 1;
+
+-- Answer: NPL
