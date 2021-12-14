4cee6c6607aacd97dd3020be97601a643baf6d74
Arnenator <andreotietgen@hotmail.com>
2021-12-14 11:29:21 +0100

    Exam preparation
    * Prepare environment for the exam
    
    * Change the max query to be correct
    
    * Add alternative or other wise correct solutions to mock exam answers

--- MockExam/Answers/Task-08.sql
+++ MockExam/Answers/Task-08.sql
@@ -17,5 +17,47 @@ GROUP BY gd.d_id
 ORDER BY COUNT(DISTINCT cd.d_country) desc
 LIMIT 1;
 
+-- Answer 582
+
+/*
+* Here the answer is correct but you should instead have tried to avoid using
+* the LIMIT 1... like this one:
+*/
+
+-- A view just like the above
+drop view if exists gAllPairs;
+create view gAllPairs
+as
+select G.d_ID as d_ID1, G.co_ID as d_ID2
+from gGarments G
+where not G.co_ID is null
+union
+select G.co_ID as d_ID1, G.d_ID as d_ID2
+from gGarments G
+where not G.co_ID is null;
+
+-- Another view that counts the contries
+drop view if exists gCountryCount;
+create view gCountryCount
+as
+select P.d_ID1 as d_ID, 
+	count(*) as d_DesignerCount, 
+	count(distinct D.d_Country) as d_CountryCount
+from gAllPairs P
+	join gDesigners D on P.d_ID2 = D.d_ID
+group by P.d_ID1;
+
+-- 7 countries, 13 designers
+select * 
+from gCountryCount CC
+where CC.d_ID = 100;
+
+-- 582
+select d_ID
+from gCountryCount CC
+where CC.d_CountryCount = (
+	select max(d_CountryCount) 
+	from gCountryCount
+);
 
 
