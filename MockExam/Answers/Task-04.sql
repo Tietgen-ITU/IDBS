8f3852dc877e997f9303fc0383d3c8a4b4129ddc
Andreas Nicolaj Tietgen <andreotietgen@hotmail.com>
2021-12-14 10:53:12 +0100

    Change the max query to be correct

--- MockExam/Answers/Task-04.sql
+++ MockExam/Answers/Task-04.sql
@@ -5,3 +5,17 @@ FROM ggarments
 GROUP BY d_id
 ORDER BY AVG(g_price) DESC
 LIMIT 1;
+
+-- How it should have been done
+
+DROP VIEW IF EXISTS AverageGarmentPrice;
+CREATE VIEW AverageGarmentPrice AS
+SELECT d_id, AVG(g_price) as avg_price
+FROM ggarments
+GROUP BY d_id;
+
+SELECT gg.d_id
+FROM ggarments gg
+GROUP BY gg.d_id
+HAVING AVG(gg.g_price) = (SELECT MAX(av.avg_price)
+                        FROM AverageGarmentPrice av);
